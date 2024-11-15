c4c31f626bf64d7be65bdf311fc802b65697f97b
Jnobel <jnobel@ivytech.edu>
2024-11-14 21:38:57 -0600

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()  # Retrieves all blog posts from the database
+    return render(request, 'blog/post_list.html', {'posts': posts})
